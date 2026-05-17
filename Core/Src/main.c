4eaed4508cf55a8303364cd5f9eabcfcf5cde524
Aeskyrr17 <kiera171717zyt@gmail.com>
2026-05-17 12:58:17 +0800

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,338 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "fdcan.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <math.h>
+#include <string.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef struct
+{
+  ADC_HandleTypeDef *hadc;
+
+  float empty_adc;
+  float weight_kg;
+  float weight_adc;
+  float val_adc;
+  float force_kg;
+} ForceSensor_t;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+//TODO: 手动砝码标定
+static ForceSensor_t force_sensor_L = {
+  .hadc = &hadc2,
+  .empty_adc = 32767.0f,
+  .weight_kg = 100.0f,
+  .weight_adc = 65535.0f,
+  .val_adc = 0.0f,
+  .force_kg = 0.0f
+};
+
+static ForceSensor_t force_sensor_R = {
+  .hadc = &hadc1,
+  .empty_adc = 32760.0f,
+  .weight_kg = 10.0f,
+  .weight_adc = 40000.0f,
+  .val_adc = 0.0f,
+  .force_kg = 0.0f
+};
+
+static volatile uint16_t adc1_dma_latest = 0;
+static volatile uint16_t adc2_dma_latest = 0;
+static volatile uint32_t adc1_sample_count = 0;
+static volatile uint32_t adc2_sample_count = 0;
+
+static uint8_t force_tx_packet[8];
+
+static volatile uint8_t force_uart_tx_ready = 1;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void ForceSensor_Init(void);
+uint8_t ForceSensor_UpdateIfNewSamples(void);
+float ForceSensor_AdcToKg(const ForceSensor_t *sensor);
+void ForceSensor_SendPacket(void);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void ForceSensor_Init(void)
+{
+  HAL_ADC_Start_DMA(force_sensor_R.hadc, (uint32_t *)&adc1_dma_latest, 1);
+  HAL_ADC_Start_DMA(force_sensor_L.hadc, (uint32_t *)&adc2_dma_latest, 1);
+
+  __HAL_TIM_SET_COUNTER(&htim6, 0);
+  HAL_TIM_Base_Start(&htim6);
+}
+
+uint8_t ForceSensor_UpdateIfNewSamples(void)
+{
+  static uint32_t last_adc1_sample_count = 0;
+  static uint32_t last_adc2_sample_count = 0;
+  uint32_t now_adc1_sample_count = adc1_sample_count;
+  uint32_t now_adc2_sample_count = adc2_sample_count;
+
+  if ((now_adc1_sample_count == last_adc1_sample_count) ||
+      (now_adc2_sample_count == last_adc2_sample_count))
+  {
+    return 0;
+  }
+
+  last_adc1_sample_count = now_adc1_sample_count;
+  last_adc2_sample_count = now_adc2_sample_count;
+
+  force_sensor_R.val_adc = (float)adc1_dma_latest;
+  force_sensor_L.val_adc = (float)adc2_dma_latest;
+
+  force_sensor_R.force_kg = ForceSensor_AdcToKg(&force_sensor_R);
+  force_sensor_L.force_kg = ForceSensor_AdcToKg(&force_sensor_L);
+
+  return 1;
+}
+
+float ForceSensor_AdcToKg(const ForceSensor_t *sensor)
+{
+  const float span_adc = sensor->weight_adc - sensor->empty_adc;
+  float force_kg;
+
+  if ((fabsf(span_adc) < 1.0f) || (sensor->weight_kg <= 0.0f))
+  {
+    return 0.0f;
+  }
+
+  force_kg = (sensor->val_adc - sensor->empty_adc)
+             * sensor->weight_kg / span_adc;
+
+  if (force_kg < 0.0f)
+  {
+    force_kg = 0.0f;
+  }
+  else if (force_kg > 100.0f)
+  {
+    force_kg = 100.0f;
+  }
+
+  return force_kg;
+}
+
+void ForceSensor_SendPacket(void)
+{
+  if (force_uart_tx_ready == 0)
+  {
+    return;
+  }
+
+  memcpy(&force_tx_packet[0], &force_sensor_L.force_kg, sizeof(force_sensor_L.force_kg));
+  memcpy(&force_tx_packet[4], &force_sensor_R.force_kg, sizeof(force_sensor_R.force_kg));
+
+  force_uart_tx_ready = 0;
+  if (HAL_UART_Transmit_DMA(&huart2, force_tx_packet, 8) != HAL_OK)
+  {
+    force_uart_tx_ready = 1;
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_ADC2_Init();
+  MX_FDCAN1_Init();
+  MX_USART2_UART_Init();
+  MX_TIM6_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
+  HAL_ADCEx_Calibration_Start(&hadc2, ADC_SINGLE_ENDED);
+
+  ForceSensor_Init();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if (ForceSensor_UpdateIfNewSamples() != 0)
+    {
+      ForceSensor_SendPacket();
+    }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 42;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+  if (hadc->Instance == ADC1)
+  {
+    adc1_sample_count++;
+  }
+  else if (hadc->Instance == ADC2)
+  {
+    adc2_sample_count++;
+  }
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART2)
+  {
+    force_uart_tx_ready = 1;
+  }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
